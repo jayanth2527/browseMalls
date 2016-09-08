c20aa92f0763c109f796eb15c968c1665cf2fbc7
r091903 <praveen.jujare@wipro.com>
2016-09-08 09:54:15 +0530

    modified view

--- public/js/frontPageScript.js
+++ public/js/frontPageScript.js
@@ -74,134 +74,149 @@ app.controller("cntr",function($http, $scope,$mdDialog){
      "mall_id":["blr-for-kor","blr-for-whi","blr-sho-kor"]
    }
  ];
+
+ // malls names
+ var mall_details=[
+ {
+   "id":"blr-for-kor",
+   "name":"Forum Mall-Koramangala",
+   "Location":"Bangalore",
+   "address":"Forum Mall,Hosur Road, Koramangala, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312"
+ },
+ {
+   "id":"blr-for-whi",
+   "name":"Forum Mall-WhiteField",
+   "Location":"Bangalore",
+   "address":"Forum Mall, WhiteField, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312",
+   "cbs":"offer_id"
+ },
+ {
+   "id":"blr-tot-mad",
+   "name":"Total Mall-Madivala",
+   "Location":"Bangalore",
+   "address":"Total Mall,Hosur Road, Madivala, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312"
+ },
+ {
+   "id":"blr-sho-kor",
+   "name":"ShopperStop-Koramangala",
+   "Location":"Bangalore",
+   "address":"Beside Forum Mall,Hosur Road, Koramangala, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312"
+ },
+ {
+   "id":"blr-pho-mah",
+   "name":"Phoenix-Mahadevapura",
+   "Location":"Bangalore",
+   "address":"Phoenix Mall, Mahadevapura, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312"
+ },
+ {
+   "id":"blr-oas-kor",
+   "name":"Oasis Mall-Koramangala",
+   "Location":"Bangalore",
+   "address":"Oasis Mall, Sony Signal, Koramangala, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312"
+ },
+ {
+   "id":"blr-cen-bel",
+   "name":"Central Mall-Bellandur",
+   "Location":"Bangalore",
+   "address":"Central Mall, Bellandur, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312"
+ },
+ {
+   "id":"blr-gop-jpn",
+   "name":"Gopalan Mall-J.P.Nagar",
+   "Location":"Bangalore",
+   "address":"Gopalan Mall, J.P.Nagar, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312"
+ },
+ {
+   "id":"blr-gop-krp",
+   "name":"Gopalan Mall-K.R.Puram",
+   "Location":"Bangalore",
+   "address":"Gopalan Mall, K.R.Puram, Bangalore, Pin-560029",
+   "contact":"080-8080808, 9083190312"
+ },
+ {
+   "id":"che-for-kor",
+   "name":"White Forum Mall-Vadapalani",
+   "Location":"Chennai",
+   "address":"Gopalan Mall, Vadapalani, Chennai ,pin-600001",
+   "contact":"044-8080808, 9083190312"
+ },
+ {
+   "id":"che-phe-vel",
+   "name":"Phoenix Mall-Velachery",
+   "Location":"Chennai",
+   "address":"Phoenix Mall, Velachery, Chennai ,pin-600001",
+   "contact":"044-8080808, 9083190312"
+ },
+ {
+   "id":"hyd-gvk-jub",
+   "name":"GVK Mall-Jubli Hills",
+   "Location":"Hyderabad",
+   "address":"GVK Mall, Jubli Hills, Hyderabad ,pin-500082",
+   "contact":"040-8080808, 9083190312"
+ },
+ {
+   "id":"hyd-ino-ban",
+   "name":"Inorbit Mall-Banjara Hills",
+   "Location":"Hyderabad",
+   "address":"Inorbit Mall, Banjara Hills, Hyderabad ,pin-500082",
+   "contact":"040-8080808, 9083190312"
+ }
+ ];
+
+
+ var malls_id_array=[];
  var location_array=[];
+ var selected_location;
+
+// location array
  if(locationData.length!=0){
    for(var i=0;i<locationData.length;i++){
      var location_name=locationData[i].location_name;
      location_array.push(location_name);
    }
  }
+
  $scope.locations=location_array;
  console.log("location array aayega");
  console.log(location_array);
 
- var malls_id_array=[];
-  var selected_location="Bangalore";
-  if(selected_location!=null){
-    for(var i=0;i<locationData.length;i++){
-      if(selected_location==locationData[i].location_name){
-        malls_id_array=locationData[i].mall_id;
-      }
-    }
-  }
 
-  // malls names
-  var mall_details=[
-  {
-    "id":"blr-for-kor",
-    "name":"Forum Mall-Koramangala",
-    "Location":"Bangalore",
-    "address":"Forum Mall,Hosur Road, Koramangala, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312"
-  },
-  {
-    "id":"blr-for-whi",
-    "name":"Forum Mall-WhiteField",
-    "Location":"Bangalore",
-    "address":"Forum Mall, WhiteField, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312",
-    "cbs":"offer_id"
-  },
-  {
-    "id":"blr-tot-mad",
-    "name":"Total Mall-Madivala",
-    "Location":"Bangalore",
-    "address":"Total Mall,Hosur Road, Madivala, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312"
-  },
-  {
-    "id":"blr-sho-kor",
-    "name":"ShopperStop-Koramangala",
-    "Location":"Bangalore",
-    "address":"Beside Forum Mall,Hosur Road, Koramangala, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312"
-  },
-  {
-    "id":"blr-pho-mah",
-    "name":"Phoenix-Mahadevapura",
-    "Location":"Bangalore",
-    "address":"Phoenix Mall, Mahadevapura, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312"
-  },
-  {
-    "id":"blr-oas-kor",
-    "name":"Oasis Mall-Koramangala",
-    "Location":"Bangalore",
-    "address":"Oasis Mall, Sony Signal, Koramangala, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312"
-  },
-  {
-    "id":"blr-cen-bel",
-    "name":"Central Mall-Bellandur",
-    "Location":"Bangalore",
-    "address":"Central Mall, Bellandur, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312"
-  },
-  {
-    "id":"blr-gop-jpn",
-    "name":"Gopalan Mall-J.P.Nagar",
-    "Location":"Bangalore",
-    "address":"Gopalan Mall, J.P.Nagar, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312"
-  },
-  {
-    "id":"blr-gop-krp",
-    "name":"Gopalan Mall-K.R.Puram",
-    "Location":"Bangalore",
-    "address":"Gopalan Mall, K.R.Puram, Bangalore, Pin-560029",
-    "contact":"080-8080808, 9083190312"
-  },
-  {
-    "id":"che-for-kor",
-    "name":"White Forum Mall-Vadapalani",
-    "Location":"Chennai",
-    "address":"Gopalan Mall, Vadapalani, Chennai ,pin-600001",
-    "contact":"044-8080808, 9083190312"
-  },
-  {
-    "id":"che-phe-vel",
-    "name":"Phoenix Mall-Velachery",
-    "Location":"Chennai",
-    "address":"Phoenix Mall, Velachery, Chennai ,pin-600001",
-    "contact":"044-8080808, 9083190312"
-  },
-  {
-    "id":"hyd-gvk-jub",
-    "name":"GVK Mall-Jubli Hills",
-    "Location":"Hyderabad",
-    "address":"GVK Mall, Jubli Hills, Hyderabad ,pin-500082",
-    "contact":"040-8080808, 9083190312"
-  },
-  {
-    "id":"hyd-ino-ban",
-    "name":"Inorbit Mall-Banjara Hills",
-    "Location":"Hyderabad",
-    "address":"Inorbit Mall, Banjara Hills, Hyderabad ,pin-500082",
-    "contact":"040-8080808, 9083190312"
-  }
-  ];
-  var mall_names_array=[];
-  if(malls_id_array.length!=0){
-    for(var i=0;i<malls_id_array.length;i++){
-       for(var j=0;j<mall_details.length;j++){
-         if(malls_id_array[i]==mall_details[j].id){
-           mall_names_array.push(mall_details[j].name);
+//************************************** location onclick function**********************************
+ $scope.locationFunction=function(location){
+    $scope.selected_location=location;
+
+    // malls list according the selected location
+     if($scope.selected_location!=null){
+       for(var i=0;i<locationData.length;i++){
+         if($scope.selected_location==locationData[i].location_name){
+           malls_id_array=locationData[i].mall_id;                   //storing comoplete mall is array into malls_id_array
          }
        }
-    }
-  }
-  $scope.filter_malls=mall_names_array;
-  console.log(mall_names_array);
+     }
+
+     var mall_names_array=[];
+     if(malls_id_array.length!=0){
+       for(var i=0;i<malls_id_array.length;i++){
+          for(var j=0;j<mall_details.length;j++){
+            if(malls_id_array[i]==mall_details[j].id){
+              mall_names_array.push(mall_details[j].name);
+            }
+          }
+       }
+     }
+     $scope.filter_malls=mall_names_array;
+     console.log("Malls names.............");
+     console.log($scope.filter_malls);
+ }
+
 
 
   // filter brands
@@ -297,32 +312,38 @@ app.controller("cntr",function($http, $scope,$mdDialog){
 
     }
   ];
-  var selected_mall="Forum Mall-Koramangala";
+  var selected_mall;
   var selected_mall_id;
   var brand_array=[];
-  if(selected_mall!=null){
-    for(var i=0;i<mall_details.length;i++){
-      if(selected_mall==mall_details[i].name){
-         selected_mall_id=mall_details[i].id;
-         console.log(selected_mall_id);
-      }
-    }
-    var mall_array=[];
-    var brand_name;
-    for(var j=0;j<brands_details.length;j++){
-      console.log("inside brands");
-      mall_array=brands_details[j].mall_id;
-      for(var k=0;k<mall_array.length;k++){
-        console.log(mall_array[k]);
-        if(selected_mall_id==mall_array[k]){
-          brand_name=brands_details[j].name;
-          brand_array.push(brand_name);
-        }
-      }
-    }//end of for second loop
-  }
-  console.log(brand_array);
-  $scope.brands=brand_array;
+
+ //**********************display brands according to selected mall**************************
+   $scope.brandFunction=function(mall){
+     selected_mall=mall;
+     if(selected_mall!=null){
+       for(var i=0;i<mall_details.length;i++){
+         if(selected_mall==mall_details[i].name){
+            selected_mall_id=mall_details[i].id;
+            console.log(selected_mall_id);
+         }
+       }
+       var mall_array=[];
+       var brand_name;
+       for(var j=0;j<brands_details.length;j++){
+         console.log("inside brands");
+         mall_array=brands_details[j].mall_id;
+         for(var k=0;k<mall_array.length;k++){
+           console.log(mall_array[k]);
+           if(selected_mall_id==mall_array[k]){
+             brand_name=brands_details[j].name;
+             brand_array.push(brand_name);
+           }
+         }
+       }//end of for second loop
+     }
+     console.log(brand_array);
+     $scope.brands=brand_array;
+   };
+
 
 
 
@@ -419,8 +440,13 @@ app.controller("cntr",function($http, $scope,$mdDialog){
       }]
     };
 
-   var selected_brand_name="Levis";
+
+   var selected_brand_name;
    var selected_brand_name_id;
+
+   $scope.offerFunction=function(brandName){
+     selected_brand_name=brandName;
+   }
    if(selected_brand_name!=null){
        for(var i=0;i<brands_details.length;i++){
            if(selected_brand_name==brands_details[i].name){
